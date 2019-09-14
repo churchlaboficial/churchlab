3f1364f0253bd4ebb7a43d2d6fb496a494a0705c
Guilherme <guilhermealves.rs@gmail.com>
2019-09-13 21:09:02 -0300

    Fix botão loggout

--- src/App.js
+++ src/App.js
@@ -284,7 +284,7 @@ class App extends React.Component {
     return(
       <div className="logout">
           <a
-           onClick={ this.userLogout.bind(this) }>Logout</a>
+           onClick={ this.userLogout.bind(this) }>Sair</a>
       </div>
     )
   }
